4047c242c7b606a4c43e7696cf6fc0f3603cb2ab
Zihang97 <jzh15@bu.edu>
2022-03-03 10:25:00 -0500

    modify device module

--- device_module.py
+++ device_module.py
@@ -5,16 +5,22 @@ def DM(json_str, filename):
 	for item in ["patientid", "temperature", "bloodpressure", "pulse", "oximeter", "weight", "height", "glucometer"]:
 		if item not in measurement_dict:
 			raise AttributeError(f"missing {item} data")
+	cnt = 0
+	if not measurement_dict["patientid"]:
+		raise ValueError("personal id can't be empty")
 	for k, v in measurement_dict.items():
 		if k != "patientid":
-			if not v:
-				raise AttributeError(f"missing {k} data")
+			if v:
+				cnt = 1
 			try:
-				v = int(v)
+				v = float(v)
 			except:
 				raise ValueError("measurement data should be numbers")
 			if v < 0:
 				raise ValueError("measurement data should be positive")
+	if cnt == 0:
+		raise ValueError("measurement data can't be empty")
+
 	with open(filename, 'w') as f:
 		json.dump(measurement_dict, f)
 
